4b2705e91068153c9f579539e2cffd11fac8d3f3
NavreenAulakh <reenaaulakh123@gmail.com>
2022-12-25 17:13:56 -0500

    Updated the search engine

--- src/index.js
+++ src/index.js
@@ -13,9 +13,8 @@ function formatDate(timestamp) {
 
   return `${day} ${hours}:${minutes}`;
 }
-function displayTemp(response) {
-  console.log(response.data);
 
+function displayTemp(response) {
   let mainTemp = document.querySelector("#main-temp");
   let cityDisplay = document.querySelector("#city-name");
   let humidity = document.querySelector("#humidity");
@@ -35,11 +34,25 @@ function displayTemp(response) {
   );
   weatherIcon.setAttribute("alt", response.data.weather[0].description);
 }
+{
+  function searchForCity(city) {
+    let cityName = city;
+    let cityHeading = document.querySelector("#city-name");
+    cityHeading.innerHTML = city;
+    let unit = "metric";
+    let apiKey = "34ae1065362d42545661451bda2b8a1f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${unit}`;
 
-let unit = "metric";
-let cityName = "Brampton";
-//let cityInput = document.querySelector("#city-input");
-//let cityName = `${cityInput.value}`;
-let apiKey = "34ae1065362d42545661451bda2b8a1f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${unit}`;
-axios.get(apiUrl).then(displayTemp);
+    axios.get(apiUrl).then(displayTemp);
+  }
+
+  function submitCity(event) {
+    event.preventDefault();
+    let cityInput = document.querySelector("#input-value");
+    let cityName = `${cityInput.value}`;
+    searchForCity(cityName);
+  }
+}
+
+let submitButton = document.querySelector("#submit-button");
+submitButton.addEventListener("click", submitCity);
